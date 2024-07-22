793d877be32a2e61e719d1939c4d9e3fcae5fb39
ARZELAYA <argeniszelaya79@gmail.com>
2024-07-21 21:37:10 -0600

    Delete update_telefonos.js

--- src/controllers/telefonos/dto/update_telefonos.js
+++
@@ -1,6 +0,0 @@
-import Joi from "joi";
-
-export const updateTelefonoShema = Joi.object().keys({
-  PI_COD_TELEFONO: Joi.number().required(),
-  PI_NUM_TELEFONO: Joi.string().length(8).default(null),
-});
