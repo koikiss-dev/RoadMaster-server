4d2c050a1d7fc3cfde42a8af6827f21e6e4c08c7
ARZELAYA <argeniszelaya79@gmail.com>
2024-07-21 21:28:45 -0600

    Delete update_sucursal.js

--- src/controllers/branch_offices/dto/update_sucursal.js
+++
@@ -1,8 +0,0 @@
-import Joi from "joi";
-
-export const updateSucursalShema = Joi.object().keys({
-  PI_COD_SUCURSAL: Joi.number().required(),
-  PV_NOM_SUCURSAL: Joi.string().default(null),
-  PI_COD_GERENTE: Joi.number().default(null),
-  PI_COD_UBICACION: Joi.number().default(null),
-});
