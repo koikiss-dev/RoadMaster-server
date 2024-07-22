8749b55714a4c7905f5b99362ee55d58ca0f9da6
ARZELAYA <argeniszelaya79@gmail.com>
2024-07-21 21:28:32 -0600

    Delete create_sucursal.js

--- src/controllers/branch_offices/dto/create_sucursal.js
+++
@@ -1,7 +0,0 @@
-import Joi from "joi";
-
-export const createSucursalcleShema = Joi.object().keys({
-  PV_NOM_SUCURSAL: Joi.string().required(),
-  PI_COD_GERENTE: Joi.number().required(),
-  PI_COD_UBICACION: Joi.number().required(),
-});
