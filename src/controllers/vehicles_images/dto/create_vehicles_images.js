2b0fad61170de3b83602aee707e6c1b529ab8c54
ARZELAYA <argeniszelaya79@gmail.com>
2024-07-21 21:51:06 -0600

    Create create_vehicles_images.js

---
+++ src/controllers/vehicles_images/dto/create_vehicles_images.js
@@ -0,0 +1,9 @@
+
+import Joi from "joi";
+
+export const createVehicleimageShema = Joi.object().keys({
+
+    PI_COD_VEHICULO: Joi.number().required(),
+    PI_COD_IMAGEN: Joi.number().required(),
+
+});
