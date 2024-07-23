07cac87e83e2b0201874ed13b1a3fe55fec372b1
yako <duronj08@gmail.com>
2024-07-22 22:28:29 -0600

    codigo de ventas revisado y con leves cambios

--- src/controllers/Ventas/routes/VentasRoutes.js
+++ src/controllers/Ventas/routes/VentasRoutes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import VentasController from "./VentasController.js";
+import VentasController from "../VentasController.js";
 
 const router = Router();
 
-router.get("/ventas", VentasController.getVentas);
-router.get("/ventas/:id", VentasController.getVenta); // Usamos :id para obtener una venta específica
-router.post("/ventas", VentasController.createVenta);
-router.put("/ventas/:id", VentasController.updateVenta); // Usamos :id para actualizar una venta específica
+router.get("/ventas", VentasController.getRegisters);
+router.get("/venta", VentasController.getRegister); // Usamos :id para obtener una venta específica
+router.post("/ventas", VentasController.createRegister);
+router.patch("/venta", VentasController.updateRegister); // Usamos :id para actualizar una venta específica
 
 export { router as VentasRoutes };
